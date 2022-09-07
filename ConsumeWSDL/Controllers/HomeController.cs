3ba5524a39d0daf99d928f2d3a4733ef010c1580
Bhupesh Sharma <bhupesh.sharma@intsof.com>
2022-09-07 20:13:07 +0530

    Commit For : An ASP.Net MVC Application which consumes the webservice hosted inside this app as service reference

---
+++ ConsumeWSDL/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeWSDL.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Status()
+        {
+            //Creating Web Service reference object  
+            WSDLServiceReference.WSDLService objPayRef = new WSDLServiceReference.WSDLService();
+            List<int> lstIntegers = new List<int> { 5, 6, 7 };
+
+            //calling and storing web service output into the variable  
+            var StatusInfo = "Output of WebService: " +objPayRef.Add(lstIntegers.ToArray()).ToString();
+            //returning josn result  
+            return Json(StatusInfo, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ WSDLServiceApp/WSDLService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSDLService.asmx.cs" Class="WSDLServiceApp.WSDLService" %>
